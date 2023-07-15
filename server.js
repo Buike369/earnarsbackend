2411880d04a943f520f0aa2174658c2c08027a7b
“Chidubem <chidubem@rawdev.com.ng>
2023-07-15 09:34:22 +0100

    cors update

--- server.js
+++ server.js
@@ -13,9 +13,32 @@ const {db} = require('./db.js')
 
 const port = process.env.PORT || 8080
 app.use(express.json())
-app.use(cors({
-    origin:"http://localhost:3000"
-}))
+
+var allowedOrigins = [
+  "http://localhost:3000",
+  "https://www.earnars.com",
+  "https://earnars.com"
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin." +
+          origin;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
+// app.use(cors({
+//     origin:"http://localhost:3000",
+
+// }))
 app.use(cookieParser())
 app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Credentials",true)
