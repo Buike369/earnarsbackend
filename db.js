6a75fad55a0941d9dde86c6cfc19ede09cae47f6
Chukwubuike Kingsley <chukwubuikekingsley369@gmail.com>
2023-07-27 09:45:27 +0100

    uploads

--- db.js
+++ db.js
@@ -2,14 +2,13 @@ const mysql = require('mysql')
 
  const db = mysql.createPool({
     connectionLimit:90,
-    host: "127.0.0.1",
-   user: "betproject",
+     host: "127.0.0.1",
+     user: "betproject",
      password: "Kingsley369",
      database: "betproject"
    
 })
 module.exports = {db}
 
-// host: "127.0.0.1",
-//    user: "betproject",
+
 
